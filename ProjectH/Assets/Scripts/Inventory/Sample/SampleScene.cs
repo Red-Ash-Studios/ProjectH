1f19d1ef51140e8dcc739fa6ababccb84f85d9b8
Halit Bedii Taner <bediitaner@outlook.com>
2024-06-11 12:42:21 +0300

    Inventory System.

---
+++ ProjectH/Assets/Scripts/Inventory/Sample/SampleScene.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using VariableInventorySystem;
+using VariableInventorySystem.Sample;
+
+public class SampleScene : MonoBehaviour
+{
+    [SerializeField] StandardCore standardCore;
+    [SerializeField] StandardStashView standardStashView;
+    [SerializeField] Button rotateButton;
+
+    void Awake()
+    {
+        standardCore.Initialize();
+        standardCore.AddInventoryView(standardStashView);
+
+        rotateButton.onClick.AddListener(standardCore.SwitchRotate);
+
+        StartCoroutine(InsertCoroutine());
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            standardCore.SwitchRotate();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            standardCore.RemoveInventoryItem(standardStashView);
+        }
+    }
+
+    IEnumerator InsertCoroutine()
+    {
+        var stashData = new StandardStashViewData(8, 16);
+
+        var caseItem = new CaseCellData(0);
+        stashData.InsertInventoryItem(stashData.GetInsertableId(caseItem).Value, caseItem);
+        standardStashView.Apply(stashData);
+
+        for (var i = 0; i < 20; i++)
+        {
+            var item = new ItemCellData(i % 6);
+            stashData.InsertInventoryItem(stashData.GetInsertableId(item).Value, item);
+            standardStashView.Apply(stashData);
+
+            yield return null;
+        }
+    }
+}
